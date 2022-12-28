c524057a1c3b420fa3735282019bd965dd98df7f
Norbert Kiesel <nk@iname.com>
2022-12-28 13:57:38 -0800

    Upgrade to Kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `java-library`
     `maven-publish`
     signing
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
     alias(libs.plugins.ktlint)
     alias(libs.plugins.detekt)
     alias(libs.plugins.versions)
